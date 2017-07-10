3b2de53169bc1ae391986eb1125ae54fc4982b27
daianef <daiane.a.fraga@gmail.com>
2017-07-10 01:31:06 -0300

    Melhorias de exibição da página
    - Divisão em seções
    - Loading
    - CSS do bootstrap
    - Mensagens de atenção
    
    Signed-off-by: daianef <daiane.a.fraga@gmail.com>

--- app/domainexplorer.rb
+++ app/domainexplorer.rb
@@ -13,24 +13,76 @@ end
 get '/search_domain' do
   @domain = params[:info]
 
-  domain_explorer = DomainExplorer.new
-  @domain_infos = domain_explorer.get_domain_information(@domain)
+  if @domain.empty?
+    @message = "Digite um domínio para pesquisar."
+    erb :empty
 
-  erb :show
+  else
+    domain_explorer = DomainExplorer.new
+    infos = domain_explorer.get_domain_information(@domain)
+
+    @domain_infos = infos[:parsed]
+    @raw = infos[:raw]
+
+    if @domain_infos.empty?
+      @message = "Nenhum resultado disponível."
+      erb :empty
+    else
+      erb :show
+    end
+
+  end
 end
 
 __END__
 
 @@ index
-<h1>Explorador de domínios</h1>
-<h3>Informe um domínio para saber informações de seu registro</h3>
-<form action="/" method="post" id="search_form">
-  <label>Domínio:</label>
-  <input name="info" type="text">
-  <button type='submit' class='btn btn-primary'>Buscar</button>
-</form>
+<div class="jumbotron">
+  <h1>Explorador de domínios</h1>
+  <p>Informe um domínio para saber detalhes sobre seu registro</p>
+  <form action="/" method="post" id="search_form">
+    <p><input name="info" type="text"></p>
+    <p><button type='submit' class='btn btn-lg btn-dafault'>Buscar</button></p>
+  </form>
+</div>
+<div id="loading"></div>
 <div id="domain""></div>
 
 @@ show
-<h3>Domínio: <%= @domain %></h3>
-<p>Infos: <%= @domain_infos %></p>
+<div class="header clearfix">
+  <h3 class="text-muted">Domínio: <%= @domain %></h3>
+</div>
+<% for section in @domain_infos %>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title"><%= section['title'] %></h3>
+    </div>
+    <div class="panel-body">
+      <% section['section_values'].each_pair do |key, value| %>
+        <% if value.respond_to? :join %>
+          <p><b><%= key %></b>:</p>
+          <ul>
+          <% for value_of_list in value %>
+            <li><%= value_of_list %></li>
+          <% end %>
+          </ul>
+        <% else %>  
+          <p><b><%= key %></b>: <%= value %></p>
+        <% end %>
+      <% end %>
+    </div>
+  </div>
+<% end %>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">Informações em formato raw (whois)</h3>
+    </div>
+    <div class="panel-body">
+      <%= @raw.gsub(/\n/, '<br />') %>
+    </div>
+  </div>
+
+@@empty
+<div class="alert alert-warning" role="alert">
+  <strong>Atenção:</strong> <%= @message %>
+</div>
